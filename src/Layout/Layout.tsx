a2426f7acad36dc8bce287344cc376bcc6d10c21
Ragina Akter <raginaakhter052@gmail.com>
2025-11-24 18:46:32 +0600

    updated home

--- src/Layout/Layout.tsx
+++ src/Layout/Layout.tsx
@@ -28,6 +28,8 @@
 
 import video from "@/assets/video/animation-Video.mp4"
 import Footer from "@/components/Home/Footer";
+
+
 import { Navbar } from "@/components/Home/Navber";
 import { Outlet } from "react-router-dom";
 
