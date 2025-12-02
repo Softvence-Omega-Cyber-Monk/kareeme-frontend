fbbe78b109b01a64ad7d0b4a8e024973f2bccfdb
EngrArfin <arfin.cse.green.edu.bd@gmail.com>
2025-12-02 17:08:03 +0600

    update demo

--- src/components/Demo.tsx
+++ src/components/Demo.tsx
@@ -1,11 +1,14 @@
-
-
 const Demo = () => {
-    return (
-        <div>
-            <h1>hi</h1>
-        </div>
-    );
+  return (
+    <div>
+      <h1>
+        hi Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint natus
+        aspernatur impedit officia cum, eos inventore iure iste, veniam,
+        corporis magnam dolorum rem numquam ducimus voluptates cumque odit
+        facilis debitis.
+      </h1>
+    </div>
+  );
 };
 
 export default Demo;
