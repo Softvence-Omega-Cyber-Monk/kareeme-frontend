405c836aa97b291ebf2327c8e9ad9aa202f523b7
EngrArfin <arfin.cse.green.edu.bd@gmail.com>
2025-12-27 12:31:03 +0600

    updaated

--- src/components/Home/demo2.tsx
+++ src/components/Home/demo2.tsx
@@ -1,11 +1,15 @@
-
-
 const demo2 = () => {
   return (
     <div>
       <h1>rtklguith</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Libero
+        consequatur hic a suscipit, veniam harum vel reprehenderit eius,
+        laboriosam quos culpa itaque? Quod praesentium rem, beatae accusantium
+        minus harum rerum.
+      </p>
     </div>
-  )
-}
+  );
+};
 
-export default demo2
+export default demo2;
