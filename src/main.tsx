bd564b316e6723f6da32742633b6addb4d7090b6
rahulkhansuvo <rahul.khan.suvo@gmail.com>
2026-02-03 15:03:42 +0600

    feat: Initialize React application entry point with routing, Redux, and cart context.

--- src/main.tsx
+++ src/main.tsx
@@ -3,23 +3,18 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import routes from "./routes/Routes.tsx";
-import { Provider } from "react-redux";
-
 import ReduxProviderWrapper from "./redux/readux-provider/reduxProviderWrapper.tsx";
 import { DashboardToaster } from "./components/ui/Toaster.tsx";
 import { CartProvider } from "./contexts/CartContext.tsx";
-import { store } from "./redux/store.ts";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Provider store={store}>
-      <ReduxProviderWrapper>
-        <CartProvider>
-          <RouterProvider router={routes} />
-          <DashboardToaster />
-        </CartProvider>
-      </ReduxProviderWrapper>
-    </Provider>
+    <ReduxProviderWrapper>
+      <CartProvider>
+        <RouterProvider router={routes} />
+        <DashboardToaster />
+      </CartProvider>
+    </ReduxProviderWrapper>
   </StrictMode>
 );
 
