e23873773f1c446825bfdc5dbe560370001348d5
rahulkhansuvo <rahul.khan.suvo@gmail.com>
2026-02-03 12:58:07 +0600

    feat: implement a new user signup page with form validation and API integration for registration.

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import React, { useState } from "react";
 import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
 import { useNavigate, Link } from "react-router-dom";
